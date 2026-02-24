d4da28e82d058725560fc1b487094e5f1c35196d
Nak1112 <savelynaku@gmail.com>
2026-02-24 21:48:04 +0100

    Update ngrok URL in script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ tg.expand();
 tg.ready();
 
 // УКАЖИТЕ ЗДЕСЬ СВОЙ URL ВАШЕГО БЭКЕНДА (FastAPI) ДЛЯ СОЗДАНИЯ ПЛАТЕЖЕЙ
-const PAYMENT_API_URL = 'http://109.120.152.239:8000/create-payment';
+const PAYMENT_API_URL = 'https://daryl-waney-shannan.ngrok-free.dev/create-payment';
 
 // ── Текущий заказ ─────────────────────────────────────────────────────────────
 
